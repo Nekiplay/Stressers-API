d70cb76cadce47fa1a20985db3d8c1e01b860027
Neki_play <neci.03@mail.ru>
2021-06-27 13:58:14 +0500

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /* APi For only browser Edge */
+            /* API For only browser Edge */
             StressersAPI.WebStress webStress = new StressersAPI.WebStress();
             Console.WriteLine(webStress.StartStrees("IP", 80, StressersAPI.WebStress.Method.CLDAP, "Login from WebSite", "Password from WebSite"));
-
+            
             StressersAPI.InstantStresser instantStresser = new StressersAPI.InstantStresser();
             Console.WriteLine(instantStresser.StartStrees("IP", 80, StressersAPI.InstantStresser.Method.CLDAP, "Login from WebSite", "Password from WebSite"));
         }
