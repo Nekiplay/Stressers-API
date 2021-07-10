8e457418b63a5defc2b1e30bec58f49a0cdb6198
Neki_play <neci.03@mail.ru>
2021-07-11 03:17:21 +0500

    Fixes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -11,11 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             /* API For only browser Edge */
-            StressersAPI.WebStress webStress = new StressersAPI.WebStress();
-            Console.WriteLine(webStress.StartStrees("IP", 80, StressersAPI.WebStress.Method.CLDAP, "Login from WebSite", "Password from WebSite"));
-            
             StressersAPI.InstantStresser instantStresser = new StressersAPI.InstantStresser();
-            Console.WriteLine(instantStresser.StartStrees("IP", 80, StressersAPI.InstantStresser.Method.CLDAP, "Login from WebSite", "Password from WebSite"));
+            Console.WriteLine(instantStresser.StartStrees("IP", 80, StressersAPI.InstantStresser.Method.CLDAP, "Nekiplay1", "125125"));
         }
     }
 }
--- StressersAPI/InstantStresser.cs
+++ StressersAPI/InstantStresser.cs
@@ -12,14 +12,14 @@ namespace StressersAPI
         {
             EdgeDriverService driverService = EdgeDriverService.CreateChromiumService();
             EdgeOptions options = new EdgeOptions();
-            driverService.HideCommandPromptWindow = true;
+            //driverService.HideCommandPromptWindow = true;
             options.UseChromium = true;
             if (proxy != null)
             {
                 options.Proxy = proxy;
             }
-            options.AddArgument("headless");
-            options.AddArgument("disable-gpu");
+            //options.AddArgument("headless");
+            //options.AddArgument("disable-gpu");
             EdgeDriver driver = new EdgeDriver(driverService, options, new TimeSpan(0, 2, 0));
             driver.Navigate().GoToUrl("https://instant-stresser.com/login");
 
@@ -29,7 +29,7 @@ namespace StressersAPI
             var passwordelemt = driver.FindElementByXPath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/form[1]/div[2]/input[1]");
             passwordelemt.SendKeys(password);
 
-            var buttonelemt = driver.FindElementByXPath("//button[@class='btn btn-primary btn-block mt-3 mb-3']");
+            var buttonelemt = driver.FindElementByXPath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/form[1]/button[1]");
             buttonelemt.Click();
             Thread.Sleep(250);
             driver.Navigate().GoToUrl("https://instant-stresser.com/hub");
@@ -40,18 +40,18 @@ namespace StressersAPI
             }
             catch { }
         }
-        public string StartStrees(string ip, int port, Method method,string login, string password, Proxy proxy = null)
+        public string StartStrees(string ip, int port, Method method, string login, string password, Proxy proxy = null)
         {
             EdgeDriverService driverService = EdgeDriverService.CreateChromiumService();
             EdgeOptions options = new EdgeOptions();
-            driverService.HideCommandPromptWindow = true;
+            //driverService.HideCommandPromptWindow = true;
             options.UseChromium = true;
             if (proxy != null)
             {
                 options.Proxy = proxy;
             }
-            options.AddArgument("headless");
-            options.AddArgument("disable-gpu");
+            //options.AddArgument("headless");
+            //options.AddArgument("disable-gpu");
             EdgeDriver driver = new EdgeDriver(driverService, options, new TimeSpan(0, 2, 0));
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
             driver.Navigate().GoToUrl("https://instant-stresser.com/login");
@@ -66,6 +66,14 @@ namespace StressersAPI
             buttonelemt.Click();
             Thread.Sleep(250);
             driver.Navigate().GoToUrl("https://instant-stresser.com/hub");
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+                var closeallelemt = driver.FindElementByXPath("//button[@class='btn btn-outline-danger btn-sm']");
+                closeallelemt.Click();
+            }
+            catch { }
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
             try
             {
                 var closeallelemt = driver.FindElementByXPath("//button[@class='btn btn-outline-danger']");
@@ -97,10 +105,14 @@ namespace StressersAPI
             var startelemt = driver.FindElementByXPath("//button[@name='startAttack']");
             startelemt.Click();
             Thread.Sleep(50);
-            var toast = driver.FindElementByXPath("//div[@class='toast-message']");
-            string st = toast.Text;
-            driver.Quit();
-            return st;
+            try
+            {
+                var toast = driver.FindElementByXPath("//div[@class='toast-message']");
+                string st = toast.Text;
+                driver.Quit();
+                return st;
+            }
+            catch { return string.Empty; }
         }
         public enum Method
         {
